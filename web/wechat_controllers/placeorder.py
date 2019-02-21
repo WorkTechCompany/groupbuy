24fe5807a70f1409dcbd792882d88bb21caad89b
任鸣 <renming@lianzai.com>
2019-02-21 16:14:16 +0800

    增加终端ip

--- web/wechat_controllers/placeorder.py
+++ web/wechat_controllers/placeorder.py
@@ -251,7 +251,7 @@ def pay():
     target_wechat =WeChatService(merchant_key=config_mina["paykey"])
 
     data = {
-        'spbill_create_ip': '172.27.0.15',
+        'spbill_create_ip': '132.232.139.186',
         'appid': config_mina['appid'],
         'mch_id': config_mina['mch_id'],
         'nonce_str': target_wechat.get_nonce_str(),
